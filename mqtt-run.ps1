1faa7bab386d4d0112fb62f4364e42ab9ff13013
Rishabh Jain <2r10j5@gmail.com>
2026-05-03 23:39:09 +0530

    Delete mqtt-run.ps1

--- mqtt-run.ps1
+++
@@ -1,9 +0,0 @@
-# MQTT run in the windows 11 using the Eclipse Mosquitto an open source MQTT broker
-
-cd D:\Mosquitto
-
-.\mosquitto.exe -c .\mosquitto.conf -v
-
-net stop mosquitto
-
-PS D:\Mosquitto> .\mosquitto_sub.exe -h 10.70.40.202 -t rfid/scan -v
